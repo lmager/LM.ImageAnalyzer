ead665c044b82b9fee857de02d8b89c2bbbf0f9b
Luuk <luuk.mager@gmail.com>
2020-05-01 14:08:29 +0200

    Web - setup HttpClient

--- LM.ImageAnalyzer.Web/Startup.cs
+++ LM.ImageAnalyzer.Web/Startup.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +20,7 @@ namespace LM.ImageAnalyzer.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton(typeof(HttpClient));
             services.Configure<AnalyserFunctionSettings>(Configuration.GetSection("AnalyzerFunction"));
         }
 
